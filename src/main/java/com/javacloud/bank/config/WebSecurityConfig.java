3fe12a16d298a509061de689838aeca9007b98fa
Deepali Patil <deepalipatil15@gmail.com>
2020-09-07 19:59:13 +0530

    feat - Add spring security as login

--- src/main/java/com/javacloud/bank/config/WebSecurityConfig.java
+++ src/main/java/com/javacloud/bank/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web
                 .ignoring()
                 .antMatchers("/h2-console/**")
+                .antMatchers("/bank/update/**")
                 .antMatchers("/bank/register/**");
     }
 }
